54628ad78a1e90f70e759f39b2e8ab8e8733197a
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-07-21 15:37:15 +0300

    landing-proto

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 // import "./style.css"
 import "./src/scss/styles.scss"
+
+const phoneInputField = document.querySelector("#phone")
+
+const phoneInput = window.intlTelInput(phoneInputField, {
+  preferredCountries: ["ru", "ua", "by", "kz"],
+
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+})
