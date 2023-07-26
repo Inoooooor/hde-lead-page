27ba1ae977bb59555e75b0593057ec4ab241743b
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-07-26 15:08:19 +0300

    added_form_submit

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ const phoneInput = window.intlTelInput(phoneInputField, {
   utilsScript:
     "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 })
+
+const error = document.querySelector(".alert-error")
+
+function process(event) {
+  event.preventDefault()
+
+  const phoneNumber = phoneInput.getNumber()
+
+  error.style.display = "none"
+
+  if (phoneInput.isValidNumber()) {
+    this.submit()
+  } else {
+    error.style.display = ""
+    error.innerHTML = `Неверный формат телефона.`
+  }
+}
+
+document.querySelector("#leadForm").addEventListener("submit", process)
