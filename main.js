5caa444836e9808115f25e27fcc84533ddf8cc05
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-07-27 13:47:05 +0300

    cleared-garbo

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 // import "./style.css"
 import "./src/scss/styles.scss"
+document.addEventListener("DOMContentLoaded", () => {
+  const phoneInputField = document.querySelector("#phone")
 
-const phoneInputField = document.querySelector("#phone")
+  const phoneInput = window.intlTelInput(phoneInputField, {
+    preferredCountries: ["ru", "ua", "by", "kz"],
 
-const phoneInput = window.intlTelInput(phoneInputField, {
-  preferredCountries: ["ru", "ua", "by", "kz"],
+    utilsScript:
+      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+  })
 
-  utilsScript:
-    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-})
+  const error = document.querySelector(".alert-error")
 
-const error = document.querySelector(".alert-error")
+  function process(event) {
+    event.preventDefault()
 
-function process(event) {
-  event.preventDefault()
+    const phoneNumber = phoneInput.getNumber()
 
-  const phoneNumber = phoneInput.getNumber()
+    error.style.display = "none"
 
-  error.style.display = "none"
-
-  if (phoneInput.isValidNumber()) {
-    this.submit()
-  } else {
-    error.style.display = ""
-    error.innerHTML = `Неверный формат телефона.`
+    if (phoneInput.isValidNumber()) {
+      this.submit()
+    } else {
+      error.style.display = ""
+      error.innerHTML = `Неверный формат телефона.`
+    }
   }
-}
 
-document.querySelector("#leadForm").addEventListener("submit", process)
+  document.querySelector("#leadForm").addEventListener("submit", process)
+})
