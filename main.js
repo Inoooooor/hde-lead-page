6e657d32ef1734e00204372df061eff844a5431f
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-08-02 11:01:25 +0300

    working-on-submit

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
-// import "./style.css"
 import "./src/scss/styles.scss"
-document.addEventListener("DOMContentLoaded", () => {
-  const phoneInputField = document.querySelector("#phone")
+import phoneVerify from "./src/utilities/phoneVerify"
+import axios from "axios"
 
-  const phoneInput = window.intlTelInput(phoneInputField, {
-    preferredCountries: ["ru", "ua", "by", "kz"],
+const form = document.querySelector("#leadForm")
 
-    utilsScript:
-      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
-  })
+form.onsubmit = async (event) => {
+  try {
+    alert(document.querySelector("#email").value)
+    return
 
-  const error = document.querySelector(".alert-error")
-
-  function process(event) {
     event.preventDefault()
-
-    const phoneNumber = phoneInput.getNumber()
-
-    error.style.display = "none"
-
-    if (phoneInput.isValidNumber()) {
-      this.submit()
-    } else {
-      error.style.display = ""
-      error.innerHTML = `Неверный формат телефона.`
+    const options = {
+      method: "POST",
+      // url: "https://api.telegram.org/bot6588888987:AAEg_Ke9HA6gRP7vGmw8nouBc8o84FhBYc8/sendMessage",
+      url: "https://webhook.site/ecf50f3a-b50b-479a-b13b-b15f5d2ab902",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      data: {
+        text: new FormData(form),
+        disable_web_page_preview: false,
+        disable_notification: false,
+        parse_mode: "HTML",
+        reply_to_message_id: null,
+        chat_id: "602753868",
+      },
     }
-  }
+    const response = await axios.request(options)
+    console.log(response)
+  } catch (error) {}
+}
 
-  document.querySelector("#leadForm").addEventListener("submit", process)
-})
+phoneVerify()
+
+console.log(form)
---
+++ src/utilities/phoneVerify.js
@@ -0,0 +1,31 @@
+export default function () {
+  document.addEventListener("DOMContentLoaded", () => {
+    const phoneInputField = document.querySelector("#phone")
+
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      preferredCountries: ["ru", "ua", "by", "kz"],
+
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+    })
+
+    const error = document.querySelector(".alert-error")
+
+    function process(event) {
+      event.preventDefault()
+
+      const phoneNumber = phoneInput.getNumber()
+
+      error.style.display = "none"
+
+      if (phoneInput.isValidNumber()) {
+        this.submit()
+      } else {
+        error.style.display = ""
+        error.innerHTML = `Неверный формат телефона.`
+      }
+    }
+
+    document.querySelector("#leadForm").addEventListener("submit", process)
+  })
+}
