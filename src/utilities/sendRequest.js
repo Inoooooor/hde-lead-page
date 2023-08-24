76fdeed0153938d360632d0ce865eb25b9277580
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-08-24 13:43:49 +0300

    added_relative_url

--- src/utilities/sendRequest.js
+++ src/utilities/sendRequest.js
@@ -1,9 +1,6 @@
 import axios from "axios"
 export default async function () {
-  const workingAddress = "-1001962937315"
-  // const testAddress = "602753868"
-  const testUrl = "https://webhook.site/0c54804b-4494-433a-af07-d79e42aaf14d"
-  const currentUrl = "https://api.telegram.org/bot6077891821:AAEtvTMyHL5VMhvyUmg8Ktnd4GZ9YPcb4qs/sendMessage"
+  const requestUrl = "/tour-form"
 
   try {
     const email = document.querySelector("#email").value
@@ -11,11 +8,10 @@ export default async function () {
     const jobPosition = document.querySelector("#jobPosition").value
     const employeesAmount = document.querySelector("#employeesAmount").value
     const region = document.querySelector(".iti__selected-flag").title
-    const chatIdAddress = workingAddress
 
     const options = {
       method: "POST",
-      url: testUrl,
+      url: requestUrl,
       headers: {
         accept: "application/json",
         "content-type": "application/json",
@@ -27,11 +23,6 @@ export default async function () {
           `\n<b>Должность:</b>\n${jobPosition}` +
           `\n<b>Количество сотрудников:</b>\n${employeesAmount}` +
           `\n<b>Регион:</b>\n${region}`,
-        disable_web_page_preview: false,
-        disable_notification: false,
-        parse_mode: "HTML",
-        reply_to_message_id: null,
-        chat_id: chatIdAddress,
       },
     }
     await axios.request(options)
